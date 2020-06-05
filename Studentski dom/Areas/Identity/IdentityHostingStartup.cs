f9b04392be20f2171f3d276415db0d6b263afda6
ekarahodza1 <ekarahodza1@etf.unsa.ba>
2020-06-05 12:14:35 +0200

    uradjen novi login

---
+++ Studentski dom/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Studentski_dom.Data;
+
+[assembly: HostingStartup(typeof(Studentski_dom.Areas.Identity.IdentityHostingStartup))]
+namespace Studentski_dom.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
